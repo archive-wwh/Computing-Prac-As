64d777e9ca5727dec6323a96f2f3f9d113255ac0
archive-wwh <archive.of.wwh@gmail.com>
2025-09-08 21:51:27 +0800

    reupload with changes

---
+++ general python.py
@@ -0,0 +1,140 @@
+#NOTE: DO NOT DEPEND ON THIS TO LEARN ABOUT PYTHON FROM SCRATCH, THIS IS ONLY FOR REFINEMENT WHEN YOU HAVE A ROUGH UNDERSTANDING
+
+#basic
+#WILL BE UPDATED SOON
+
+#slightly advanced
+#built in data types
+
+Tuple = (a,b) #Immutable (cannot change elements inside) list
+
+
+#Formatting  
+
+#F-string  
+
+formatted_text = f"text {variable:.2f}" # :2.f to round float to 2dp
+
+#Dot format 
+
+formatted_text = "text {:.2f}".format(variable) #remove :.2f if don't need convert variable to 2dp 
+
+#Alignment 
+
+formatted_text = f"{text:<n}" #</>/^ means left/right/centre respectively, n is length of text 
+
+
+#math operations
+
+% #modulo; gives remainder
+
+// #floor division; gives quotient
+
+
+#random module
+
+integer_inclusive_both_ends = random.randint(a,b) 
+
+integer_inclusive_start_exclusive_end = random.randrange(a,b) #similar to range
+
+
+#List slicing 
+
+List[i:j] #give index i to index (j - 1) [similar to range()] 
+
+List[i:j:k] #give index i to index (j - 1) with step k 
+
+
+#Array (using python list)
+
+array_of_j_integers = [0 for i in range(j)] #change 0 to "" for string
+
+#2d array
+
+2d_array_with_rows_and_columns = [[ "0" for i in range (columns)] for j in range (rows)]  
+
+item_in_2d_array = array[row][column] #floor (row), unit(column)
+
+#Pretty print:
+
+for row in array: #need both for loops for 2d arrays
+
+    output = "" 
+
+    for cell in row: #only need this for loop for 1d array
+
+        output += str(cell) + " "  
+
+    print(output) 
+
+
+#File
+
+#reading
+
+file = open("full name of file (with file extension)", "r") 
+
+
+file.readline() #ignore heading if needed
+
+
+data = [] 
+
+ 
+for line in file: 
+
+    line = line.strip() 
+
+    line = line.split(",")
+
+    #if line != “": for blank lines (without data)
+
+    data.append(line) 
+
+
+file.close() 
+
+#csv file, when "data"
+
+import csv 
+
+file = open("full name of file","r") 
+
+data = list(csv.reader(file)) 
+
+file.close() 
+
+
+#writing
+
+file = open("file name.extension", "w") #will erase content if file already exist
+#file = open("existing file name.extension","a") if add onto existing file
+
+file.write(str(contents) + "\n")  
+
+file.close() 
+
+
+#Commands  
+
+line = file.read(n) #reads first n characters of file 
+
+line = file.readline() #reads contents of entire line (reads first line if called file not read yet, etc) 
+
+line.lstrip() #strip (remove white space character) at line’s left side [vice versa for line.rstrip()]
+
+
+#turtle (a bit useless for a level h2 computing)
+#draw in shell/ console
+import turtle #import turtle library
+turtle_object = turtle.Turtle() #create object
+turtle_object.shape('turtle') #change to turtle shape
+turtle_object.shapesize(1) #change size
+turtle_object.color('red') #change colour
+turtle_object.forward(50) #move forward by 50 units
+turtle_object.left(90) #turn by 90 degrees
+turtle_object.forward(30) #complete second side of a rectangle
+
+ 
+
+  
